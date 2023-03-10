72db257506b86b6154a872d865cd4cac1a316d18
hastman1687 <hastamanjan@yahoo.com>
2023-03-10 16:20:19 +0500

    app

---
+++ app.js
@@ -0,0 +1,37 @@
+var incre=document.getElementById("num")
+var show=document.getElementById("save")
+var count=0;
+
+
+function add(){
+count+=1
+incre.innerHTML=count
+}
+
+
+
+function save1(){
+
+    show.innerHTML=count;
+
+
+}
+
+
+
+
+// method number 2
+
+
+// var inc = document.getElementById("num");
+// var save = document.getElementById("save");
+// var count = 0;
+
+// function add() {
+//     count += 1;
+//     inc.innerText = count;
+// }
+
+// function save1() {
+//     save.innerHTML = inc.innerText; change this part
+// }
